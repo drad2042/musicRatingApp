883a1b48a42f8f3689132ac9f72d58262e6fa4cc
drad2042 <dradunsky2042@gmail.com>
2024-01-28 00:29:57 -0500

    creating timer in main / printing artist name

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from webScraping import Scraper
+import time
 s = Scraper()
 
 ## USES DEEZER API: https://developers.deezer.com/api/album
 
 def main():
+    start = time.time()
     s.scrapeByArtist()
+    end = time.time()
+    totalTime = end - start
+    print(totalTime)
 
 main()
     
--- webScraping.py
+++ webScraping.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import lxml
+import json
 
 class Scraper:
     def __init__(self):
@@ -8,7 +10,12 @@ class Scraper:
 
     def scrapeByArtist(self): #Scrapes data off deezer by artist
         URL = "https://api.deezer.com/artist/"
-        for i in range(1,2):
+        for i in range(1,100):
             r = requests.get(URL + str(i))
-            soup = BeautifulSoup(r.content, 'html5lib')
-        
+            
+            soup = BeautifulSoup(r.content, 'lxml').text  
+            soup = json.loads(soup)
+            try:
+                print(soup["name"])
+            except:
+                continue
