2553ca09b063266426bb741ba9fa32c1b434aecd
dfr5407 <dfr5407@psu.edu>
2024-06-21 12:28:52 -0400

    scrape files

--- scrape.py
+++ scrape.py
@@ -1,3 +1,19 @@
 import requests
 import pandas as pd
+import secrets
+from dotenv import load_dotenv
+import os
+
+
+
+class Data_Scrape:
+    def __init__(self):
+
+        load_dotenv()
+        self.api_key = os.getenv("API_KEY")
+        self.api_secret = os.getenv("API_SECRET")
+
+
+c = Data_Scrape()
+print(c)
 
